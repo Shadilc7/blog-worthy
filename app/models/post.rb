c438b35ffefd226406229a6a3ef18e56721677e8
shadil <shadilc7@gmail.com>
2024-09-17 19:38:07 +0530

    feat: add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  MAX_TITLE_LENGTH = 12
+  validates :title, presence: true, length: {maximum: MAX_TITLE_LENGTH}
+  validates_inclusion_of :is_blog_worthy, in: [true, false]
+  validate :is_negative?
+
+  def is_negative?
+    errors.add(:base, "Difference between upvote and downvote can't be negative") if upvote - downvote < 0
+  end
 end
